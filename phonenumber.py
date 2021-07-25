99ff5719b195b266c73ccd3fd1d60ffcd66fab82
POKMAN2018 <57592691+POKMAN2018@users.noreply.github.com>
2021-07-25 15:57:45 +0700

    Add files via upload

---
+++ phonenumber.py
@@ -0,0 +1,16 @@
+﻿import phonenumbers as phone
+
+user = int(input("ใส่เบอร์ดทณไม่ต้องมี0 : "))
+number = str(user)
+
+x = phone.parse("+66"+number, None)
+
+from phonenumbers import geocoder
+
+print(geocoder.description_for_number(x, "th"));
+
+from phonenumbers import carrier
+print(carrier.name_for_number(x, "th"))
+
+from phonenumbers import timezone
+print(timezone.time_zones_for_number(x))
